389e996cd635cae022c30cccb8805ae6083eca0c
dhanushswamy <dhanushswamy25@gmail.com>
2024-03-28 09:39:00 +0530

    updated first commit

--- src/javapracticeprograms/Prog1.java
+++ src/javapracticeprograms/Prog1.java
@@ -7,6 +7,8 @@ public class Prog1 {
 	public static void main(String[] args) {
 		String str="suzysimbapluto";
 		HashMap<Character,Integer> hmap=new HashMap<Character,Integer>();
+
+		
 		int lengthofstring=str.length();
 		for(int i=0;i<lengthofstring;i++)
 		{
